8413e3d837a8f3f2b78ffb8bb6a288a55db3e766
gustavoamorimm <guhx.amorim@gmail.com>
2024-05-26 00:21:10 -0300

    Capitulo 2 Finalizado

--- src/projetoIntegrador/OperacaoAurora.java
+++ src/projetoIntegrador/OperacaoAurora.java
@@ -58,21 +58,20 @@ public class OperacaoAurora {
 
 	// #region Funçõs responsáveis pelo desenvolvimento dos capitúlos/jogo.
 
-	/*
-	 * Função responsável por iniciar o PRIMEIRO iro capitúlo.
-	 */
+	
+//	  Função responsável por iniciar o PRIMEIRO capitúlo.
+	 
 	static void SombrasGrandeMaca() {
 		System.out.println("\nSOMBRAS DA GRANDE MAÇA: ");
 	}
 
-	/*
-	 * Função responsável por iniciar o SEGUNDO capitúlo.
-	 */
+	
+//	 Função responsável por iniciar o SEGUNDO capitúlo.
+	 
 	static void DesvendarOlhoSecreto() {
 		
-		
-		System.out.println("\nCapitulo 2:");
-		System.out.println("\n");
+		System.out.println("Capitulo 2:\n");
+		System.out.println();
 		System.out.println(
 		            "██████╗ ███████╗███████╗██╗   ██╗███████╗███╗   ██╗██████╗  █████╗ ███╗   ██╗██████╗  ██████╗ \n" +
 		            "██╔══██╗██╔════╝██╔════╝██║   ██║██╔════╝████╗  ██║██╔══██╗██╔══██╗████╗  ██║██╔══██╗██╔═══██╗\n" +
@@ -88,79 +87,195 @@ public class OperacaoAurora {
 		            " ╚═════╝      ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝     ╚══════╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝   ╚═╝    ╚═════╝    "
 		        );
 		
-		System.out.println("\n");
-		System.out.println("\nApós Ethan conferir quase todos os documentos encontrados dentro do compartimento secreto.");
-		System.out.println("\nEle encontra um ultimo documento, então Ethan resolve ler esse documento, nele estava escrito algumas informações sobre seus pais e que eles eram dono de uma organização secreta conhecida como O Olho Secreto");
-	    System.out.println("\nUma organização secreta obscura especializada em segurança nacional e em inteligência liderada pelo seus pais que,aparentemente,foram vítimas de uma traição dentro da própria organização.");
-		System.out.println("\nA revelaçào acaba sendo desvastadora para Ethan, o que acaba fazendo com que ele ficasse muito confuso e devastado com tudo que ele tinha lido no documento.Então Ethan se vê diante de um dilema cruel:");
-		System.out.println("\nSeguir em frente com sua vida ou embarcar em uma jornada perigosa para desvendar a verdade sobre seus pais e a organização obscura à qual eles pertenciam:");
+		System.out.println();
+		System.out.println("Após Ethan conferir quase todos os documentos encontrados dentro do compartimento secreto.\n");
+		System.out.println("Ele encontra um ultimo documento, então Ethan resolve ler esse documento, nele estava escrito algumas informações sobre seus pais e que eles eram dono de uma organização secreta conhecida como O Olho Secreto.\n");
+	    System.out.println("Uma organização secreta obscura especializada em segurança nacional e em inteligência liderada pelo seus pais que,aparentemente,foram vítimas de uma traição dentro da própria organização.\n");
+		System.out.println("A revelaçào acaba sendo desvastadora para Ethan, o que acaba fazendo com que ele ficasse muito confuso e devastado com tudo que ele tinha lido no documento.Então Ethan se vê diante de um dilema cruel.\n");
+		System.out.println("Seguir em frente com sua vida ou embarcar em uma jornada perigosa para desvendar a verdade sobre seus pais e a organização obscura à qual eles pertenciam:\n");
 		
 		int decisao;
 		do {
-			System.out.println("\n1 - Seguir em frente com sua vida e ignorar tudo que tenha lido");
+			System.out.println("1 - Seguir em frente com sua vida e ignorar tudo que tenha lido\n");
             System.out.println("2 - Embarcar em uma jornada perigosa para desvendar a verdade sobre seus pais e a organização obscura à qual eles pertenciam.");
             
             decisao = input.nextInt();
             
             if (decisao == 1) {
-                System.out.println("\nVocê decidiu seguir em frente com sua vida e ignorar tudo que você leu. Mas será que essa é realmente um boa descisão!");
+                System.out.println("Você decidiu seguir em frente com sua vida e ignorar tudo que você leu. Mas será que essa é realmente um boa descisão!\n");
             } else if (decisao == 2) {
-                System.out.println("Você escolheu embarcar em uma jornada perigosa em busca da verdade sobre seus pais e a organização obscura.");
+                System.out.println("Você escolheu embarcar em uma jornada perigosa em busca da verdade sobre seus pais e a organização obscura.\n");
                 
             } else {
                 System.out.println("Opção inválida. Por favor, escolha apenas [1] ou [2]:");
             }
-        
+    
+            
 		} while (decisao != 2);
 	 
-		System.out.println("A sede de justiça e a busca por respostas o impulsionam a seguir em frente. Você decide infiltrar-se mo O Olho Secreto e desvendar seus segredos e descobrir quem foi o responsável pela morte de seus pais");
-		System.out.println("A missão vai ser árdua e repleta de perigos, mas Ethan está determinado a fazer o que for preciso para obter justiça e honrar a memória de seus pais.");
+		System.out.println("A sede de justiça e a busca por respostas o impulsionam a seguir em frente. Ethan decide infiltrar-se no Olho Secreto e desvendar seus segredos e descobrir quem foi o responsável pela morte de seus pais.\n");
+		System.out.println("A missão vai ser árdua e repleta de perigos, mas Ethan está determinado a fazer o que for preciso para obter justiça e honrar a memória de seus pais.\n");
+		System.out.println("Ethan: acaba se decidindo,ele precisava encontrar equipamentos para conseguir se enfiltrar,então ethan decide ir visitar um local que ele já havia passado antes após interrogar alguns informantes.\n");
+		System.out.println("Esse local era obscuro, um lugar onde  criminosos, bandidos,guangues frequentavam  um local clandestino que fica escondido em sin city, Ethan caminhava e observava as pessoas ao seu redor, olhava para o chão sujo com poças de aguá e as paredes com rachaduras.\n");
+		System.out.println("Ethan continua caminhando até chegar na loja, Ethan entra na loja e encontra um senhor barbudo e careca, o vendedor pegunta para ethan\n");
+		System.out.println("???:O que você procura rapaz?\n");
+		
+		int escolha;
+		do {
+			System.out.println("1 - Responder ao vendendor que está procurando equipamentos e roupas");
+            System.out.println("2 - Perguntar o nome do vendedor");
+            
+            escolha = input.nextInt();
+            input.nextLine();
+            
+            if (escolha == 1) {
+                System.out.println("Estou procurando alguns equipamentos e roupas, preciso ir em uma missão.");
+            } else if (escolha == 2) {
+                System.out.println("Ethan: olha para o vendedor e pergunta como ele se chamava.");
+                System.out.println("???: O vendendor olha para o rosto de Ethan e responde, vejo que você não é desse lugar.");
+                System.out.println("Ethan: pergunta como ele sabia dessa informação?");
+                System.out.println("???: o vendedor responde que ele não tinha aparencia de um criminoso.");
+                System.out.println("Ethan: responde que ele realmente não fazia parte desse lugar obscuro e sujo, que estava só de passagem.");
+                System.out.println("???: O vendedor dá um risada e fala bom não tenho interesse em saber quais são seus motivos para você estar visitando esse lugar.");
+                System.out.println("???: o pessoal por aqui me chama de Victor.");
+                System.out.println("Ethan: estou procurando alguns equipamentos e roupas victor, preciso ir em uma missão.");
+                
+            } else {
+                System.out.println("Opção inválida. Por favor, escolha entre a opção [1] ou [2]:");
+            }
+		
+		} while (escolha != 1 && escolha != 2 );
+		
+		System.out.println("você está na loja certa tenho alguma roupas e equipamentos do que você precisa.");
+		System.out.println("Ethan: preciso de um traje apropriado, uma mascara, uma identidade falsa e uma arma.");
+		System.out.println("Vou providenciar para você espere um pouco, aqui está tudo o que você pediu era isso que você precisava?");
+		System.out.println("Ethan: sim está tudo certo, pode ficar com o troco.");
+		System.out.println("O vendendor fala para ethan, tome cuidado e vê se não morre em sua missão, quem sabe você a gente não possa tomar um cerveja e você me conta sobre sua missão");
+		System.out.println("Ethan: Quem sabe, mas espero que eu não tenha que voltar para esse lugar denovo");
+		System.out.println("O vendedor olha para Ethan e dá um risada");
+		System.out.println("Ethan sai da loja e resolve ir até a base do olho secreto\n");
+
+		
+//		Enigma 2: O Código Secreto:
+		
+		System.out.println("Ethan chega até a base do olho secreto e consegue se infiltrar na base sem ser visto pelos guardas");
+		System.out.println("Ao se infiltrar na base ethan se depara com um computador protegido por um código complexo.");
+		System.out.println("O acesso a porta está bloqueado e tudo depende da decifração do código, que parece ser um criptograma elaborado.\n");
+		
+
+        String respostaCorreta = "A"; // a resposta correta letra A
+
+        while (true) {
+            System.out.println("Qual é o resultado de 2^3 * 2^2?");
+            System.out.println("A) 2^5");
+            System.out.println("B) 2^6");
+            System.out.println("C) 2^4");
+
+            String respostaUsuario = input.nextLine();
+
+            if (respostaUsuario.equals(respostaCorreta)) {
+                System.out.println("Você conseguiu destravar a porta!/n");
+                break;
+            } else {
+                System.out.println("Tente novamente.");
+            }
+        }
+		
 	}
 	
+
+	//	Função responsável por iniciar o TERCEIRO capitúlo.
+	 
+
+		static void BuscarVerdade() throws Exception {
+			System.out.println("Capitulo 3:\n");
+			System.out.println();
+			System.out.println(
+					"█████╗     ███╗   ███╗███████╗███╗   ██╗███████╗ █████╗  ██████╗ ███████╗███╗   ███╗     ██████╗  ██████╗██╗   ██╗██╗  ████████╗ █████╗    \n" +
+					"██╔══██╗    ████╗ ████║██╔════╝████╗  ██║██╔════╝██╔══██╗██╔════╝ ██╔════╝████╗ ████║    ██╔═══██╗██╔════╝██║   ██║██║  ╚══██╔══╝██╔══██╗ ██╗\n" +
+					"███████║    ██╔████╔██║█████╗  ██╔██╗ ██║███████╗███████║██║  ███╗█████╗  ██╔████╔██║    ██║   ██║██║     ██║   ██║██║     ██║   ███████║ ╚═╝\n" +
+					"██╔══██║    ██║╚██╔╝██║██╔══╝  ██║╚██╗██║╚════██║██╔══██║██║   ██║██╔══╝  ██║╚██╔╝██║    ██║   ██║██║     ██║   ██║██║     ██║   ██╔══██║ ██╗\n" +
+					"██║  ██║    ██║ ╚═╝ ██║███████╗██║ ╚████║███████║██║  ██║╚██████╔╝███████╗██║ ╚═╝ ██║    ╚██████╔╝╚██████╗╚██████╔╝███████╗██║   ██║  ██║ ╚═╝\n" +
+					"╚═╝  ╚═╝    ╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝     ╚═════╝  ╚═════╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝  ╚═╝   "
+					);
+			
+			System.out.println();
+			System.out.println("Através de sua inteligência e perspicácia, Ethan consegue se infiltra dentro do Olho Secreto");
+			System.out.println("Ethan começa a caminhar pelos corredores e logo vê dois guardas na sua frente parados conversando");
+			System.out.println("Ethan precisava dar um jeito de passar pelos dois guardas para que ele podesse continuar, então ele resolve enfrentar os dois guardas");
+			System.out.println("Ethan pega sua arma e resolve atirar nos dois guardas, ele estava um pouco nevorso e sua mira não estava muito boa\n");
+			
+//			Conta para acerta os guardas
+
+			String[] opcoes = {"A: 196", "B: 729", "C: 512"};  //	resposta correta letra B
+			int vidas = 3;
+			boolean guardasVivos = true;
+
+			while (guardasVivos && vidas > 0) {
+			    System.out.println("Resolva a equação: (3^2)^2 = ?");
+			    for (int i = 0; i < opcoes.length; i++) {
+			        System.out.print(opcoes[i] + " ");
+			    }
+			    System.out.println();
+			    String alternativa = input.next();
+
+			    if (alternativa.equalsIgnoreCase("B")) {
+			        System.out.println("Você acertou! Você matou os dois guardas com dois tiros.");
+			        guardasVivos = false;
+			    } else {
+			        vidas--;
+			        if (vidas == 2) {
+			            System.out.println("Você errou! Você foi atingido pelos guardas de raspão e perdeu uma vida.");
+			        } else if (vidas == 1) {
+			            System.out.println("Você errou novamente! Você foi atingido com um tiro na perna e perdeu outra vida.");
+			        } else {
+			            System.out.println("Você errou pela terceira vez! Você foi acertado pelos guardas na cabeça e perdeu todas as vidas.\n");
+			            Menu();  
+			        }
+			    }
+			}
+	        
+			System.out.println("Após ethan consegui matar os dois guardas, ele segue em frente pelos corredores");
+			System.out.println("Ethan caminha pelos corredores e encontra um pessoa na sua frente, esse inviduo parecia diferente");
+			
+
+		}
+
 	
-
-	/*
-	 * Função responsável por iniciar o TERCEIRO capitúlo.
-	 */
-	static void BuscarVerdade() {
-		System.out.println("\nBUSCAR A VERDADE POR TRÁS DE TUDO:");
-	}
-
-	/*
-	 * Função responsável por iniciar o QUARTO capitúlo.
-	 */
+//	  Função responsável por iniciar o QUARTO capitúlo.
+	 
 	static void Alianca() {
 		System.out.println("\nALIANDO-SE A ALGUÉM IMPROVAVEL:");
 	}
 
-	/*
-	 * Função responsável por iniciar o QUINTO capitúlo.
-	 */
+	
+//	  Função responsável por iniciar o QUINTO capitúlo.
+	 
 	static void EncontrarAurora() {
 		System.out.println("\nBUSCANDO A AURORA: ");
 	}
 
-	/*
-	 * Função responsável por iniciar o SEXTO capitúlo.
-	 */
+	
+//	  Função responsável por iniciar o SEXTO capitúlo.
+	 
 	static void Confrontar() {
 		System.out.println("\nO CONFRONTO FINAL - TERÁ OS 3 FINAIS: ");
 	}
 	// #endregion
 
-	/*
-	 * Função responsável por iniciar o jogo.
-	 */
-	static void Jogar() throws InterruptedException {
+	
+//	  Função responsável por iniciar o jogo.
+	 
+	static void Jogar() throws Exception {
 		// Colocar um letreiro maior (pegar no google) para aparecer no console.
 		System.out.println("SEJA BEM VINDO A OPERAÇÃO AURORA!");
 
 		Imprimir("Preparado (a) para uma aventura nunca antes vivida? Aperte os cintos e vamos lá!",
 				TimeUnit.MILLISECONDS, tempo_dialogo);
 
-		/*
-		 * Inicializando os capitúlos do jogo e seus respectivos aspectos.
-		 */
+		
+//		  Inicializando os capitúlos do jogo e seus respectivos aspectos.
+		 
 
 		// 1 - capitulo
 		SombrasGrandeMaca();
