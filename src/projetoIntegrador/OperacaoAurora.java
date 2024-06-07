d1ff784f3292114f5003fa345c22a4bd0faa2594
Mariana Medeiros <marianacostamedeiros1@gmail.com>
2024-06-07 07:56:00 -0300

    - Ajustando tempo de narrativa; - Construindo o capitulo 1: Sombras da Grande Maca; - Adicionando letreiro de bem vindo ao iniciar o jogo; - Ajuste de nomenclatura.

--- src/projetoIntegrador/OperacaoAurora.java
+++ src/projetoIntegrador/OperacaoAurora.java
@@ -16,7 +16,7 @@ public class OperacaoAurora {
 	 * Variáveis responsáveis por determinar o delay nas mensagens que serão
 	 * imprimidas.
 	 */
-	static int tempo_dialogo = 70, tempo_narrativa = 150, tempo_transicao = 25;
+	static int tempo_dialogo = 70, tempo_narrativa = 60, tempo_transicao = 25;
 
 	// #region Funções genéricas para utilização de forma global.
 
@@ -57,8 +57,7 @@ public class OperacaoAurora {
 
 	// #region Funçõs responsáveis pelo desenvolvimento dos capitúlos/jogo.
 
-//	  Função responsável por iniciar o PRIMEIRO capitúlo.
-
+	// Função responsável por iniciar o PRIMEIRO capitúlo.
 	static void SombrasGrandeMaca() throws Exception {
 		Imprimir("\n\nCapitulo 1: \n\n",TimeUnit.MILLISECONDS, tempo_narrativa);
 		
@@ -69,7 +68,120 @@ public class OperacaoAurora {
 				+"╚════██║██║   ██║██║╚██╔╝██║██╔══██╗██╔══██╗██╔══██║╚════██║    ██║  ██║██╔══██║    ██║   ██║██╔══██╗██╔══██║██║╚██╗██║██║  ██║██╔══╝      ██║╚██╔╝██║██╔══██║██║     ██╔══██║\n"
 				+"███████║╚██████╔╝██║ ╚═╝ ██║██████╔╝██║  ██║██║  ██║███████║    ██████╔╝██║  ██║    ╚██████╔╝██║  ██║██║  ██║██║ ╚████║██████╔╝███████╗    ██║ ╚═╝ ██║██║  ██║╚██████╗██║  ██║\n"
 				+"╚══════╝ ╚═════╝ ╚═╝     ╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝    ╚═════╝ ╚═╝  ╚═╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝    ╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝\u001B[0m");
+				
+		System.out.println();
+		Imprimir("Sin City, 1925. A cidade que nunca dorme pulsa com vida e energia, um caldeirão de sonhos, ambições e segredos obscuros. ",TimeUnit.MILLISECONDS, tempo_narrativa);
+		Imprimir("Nas ruas movimentadas e becos escuros, a sombra da tragédia paira sobre Ethan Blackwood, um jovem atormentado pela morte misteriosa de seus pais. ",TimeUnit.MILLISECONDS, tempo_narrativa);
+		Imprimir("As lembranças das últimas horas com eles ainda estão frescas em sua mente: a risada contagiante de sua mãe, a voz firme de seu pai, o aconchego do lar. ",TimeUnit.MILLISECONDS, tempo_narrativa);
+		Imprimir("De repente, tudo se desfez em uma noite brutal, deixando apenas um rastro de perguntas sem respostas e um vazio imenso em seu coração.\n\n",TimeUnit.MILLISECONDS, tempo_narrativa);
+
+		Imprimir("Um bar clandestino com iluminação fraca e música de jazz ao fundo. Ethan Blackwood, de aparência abatida, está sentado no balcão, segurando um copo de whisky. O barman, um homem de meia-idade com um olhar astuto, se aproxima.",TimeUnit.MILLISECONDS, tempo_narrativa);		
+		Imprimir("Um bar clandestino, com iluminação fraca e música de jazz ao fundo. Ethan Blackwood, de aparência abatida, está sentado no balcão, segurando um copo de whisky. O barman, um homem de meia-idade com um olhar astuto, se aproxima.\n\n",TimeUnit.MILLISECONDS, tempo_narrativa);		
 		
+		Imprimir("Barman: (enxugando um copo) Você tem andado pensativo, rapaz. O que te traz a este canto esquecido da cidade? \n",TimeUnit.MILLISECONDS, tempo_dialogo);		
+		Imprimir("Ethan Blackwood: (olhando para o copo, com a voz baixa) Estou atrás de respostas. Meus pais... morreram de uma maneira que ninguém consegue explicar. Acho que alguém aqui sabe de algo. \n",TimeUnit.MILLISECONDS, tempo_dialogo);		
+		Imprimir("Barman: (arqueando uma sobrancelha) Sin City tem um jeito de engolir segredos. Mas cuidado onde pisa, garoto. Às vezes, as respostas que buscamos são piores que as perguntas. \n\n",TimeUnit.MILLISECONDS, tempo_dialogo);		
+		
+		Imprimir("Ethan Blackwood, em um beco escuro, confrontando um informante, um homem magro e nervoso chamado Jimmy. \n\n",TimeUnit.MILLISECONDS, tempo_narrativa);		
+		
+		Imprimir("Ethan Blackwood: (segurando o colarinho de Jimmy) Você sabe algo sobre a noite em que meus pais morreram. Fale, ou vai se arrepender de ter nascido...",TimeUnit.MILLISECONDS, tempo_dialogo);		
+		Imprimir("Jimmy: (tentando se soltar, com medo nos olhos) Ok, ok! Tudo o que sei é que eles estavam envolvidos com algo grande. Ouvi dizer que um tal de Aurora estava no centro disso tudo. \n\n",TimeUnit.MILLISECONDS, tempo_dialogo);		
+		
+		Imprimir("No apartamento de Ethan. Ele está sentado em sua escrivaninha, examinando o medalhão de prata antigo. Ao tocá-lo, uma mensagem holográfica surge. \n",TimeUnit.MILLISECONDS, tempo_narrativa);		
+		Imprimir("Mensagem Holográfica: Aurora... Se você está vendo isso, significa que chegamos ao fim de nossa jornada. O caminho para a verdade está no mapa escondido. Decifre-o e encontrará a chave para o próximo passo.\n\n",TimeUnit.MILLISECONDS, tempo_dialogo);		
+		
+		Imprimir("Ethan está em sua sala, com documentos espalhados por todo lado. Entre eles, um mapa antigo com símbolos e marcas estranhas. \n\n",TimeUnit.MILLISECONDS, tempo_narrativa);		
+		
+		Imprimir("Ethan Blackwood: (falando para si mesmo) O mapa... preciso entender esses símbolos. Talvez a história da região possa me dar alguma pista.\n\n",TimeUnit.MILLISECONDS, tempo_dialogo);		
+		
+		Imprimir("Biblioteca de Sin City. Ethan está rodeado por livros antigos, folheando um deles com intensidade. \n\n",TimeUnit.MILLISECONDS, tempo_narrativa);		
+		
+		Imprimir("Ethan Blackwood: (pensando alto) Esses símbolos... parecem ter algum tipo de ligação com antigos rituais. Preciso encontrar um local histórico que bata com essas marcas.\n\n",TimeUnit.MILLISECONDS, tempo_dialogo);		
+		
+		Imprimir("Ethan encontra um mapa antigo e envelhecido, com símbolos e marcas estranhas. Após examinar o mapa e os documentos, ele percebe que as coordenadas para o local oculto estão codificadas em uma mensagem matemática. \n",TimeUnit.MILLISECONDS, tempo_narrativa);		
+		Imprimir("A mensagem matemática possui uma série de polinômios ao lado dos símbolos no mapa. Após estudar os documentos, ele descobre que cada polinômio corresponde a uma coordenada específica. \n",TimeUnit.MILLISECONDS, tempo_narrativa);		
+		Imprimir("A mesagem diz: 'A chave está nas raízes dos polinômios, encontre o ponto onde o sol se põe.' \n\n",TimeUnit.MILLISECONDS, tempo_narrativa);		
+		
+		Imprimir("Mensagem Decifrada: \n",TimeUnit.MILLISECONDS, tempo_narrativa);		
+		Imprimir("1 - Símbolo: Polinômio P(x) = x² - 5x + 6\n",TimeUnit.MILLISECONDS, tempo_narrativa);		
+		Imprimir("2 - Símbolo: Polinômio Q(x) = x² - 7x + 10\n\n",TimeUnit.MILLISECONDS, tempo_narrativa);		
+		
+		Imprimir("Ethan Blackwood: (pensando com ele mesmo) Preciso encontrar as raízes dos polinômios... Isso me dará as coordenadas do local! \n\n",TimeUnit.MILLISECONDS, tempo_dialogo);		
+		
+		Imprimir("Coordenadas: \n",TimeUnit.MILLISECONDS, tempo_dialogo);		
+		
+        String[] respostas = {
+            "2.2° e 3.5°",  // Resposta correta
+            "1.5° e 3.6°",
+            "2.2° e 3.2°",
+            "3.5° e 2.1°"
+        };
+
+        String[] letras = {"A", "B", "C", "D"};
+		
+		// Variáveis para controle das vidas
+        int vidas = 3;
+        boolean respostaCorreta = false;
+		
+        while (!respostaCorreta) {
+            // Embaralha as alternativas
+            for (int i = 0; i < respostas.length; i++) {
+				int j = random.nextInt(respostas.length);
+
+                String temp = respostas[i];
+                respostas[i] = respostas[j];
+                respostas[j] = temp;
+            }
+
+            // Exibe as alternativas embaralhadas
+            for (int i = 0; i < respostas.length; i++) {
+                Imprimir(letras[i] + ") " + respostas[i] + "\n",TimeUnit.MILLISECONDS, tempo_transicao);
+            }
+            
+            System.out.println();
+            Imprimir("Digite a letra da resposta correta: ",TimeUnit.MILLISECONDS, tempo_transicao);
+            
+            String resposta = input.nextLine().toUpperCase();
+
+            int indexResposta = -1;
+            switch (resposta) {
+                case "A":
+				indexResposta = 0;
+                    break;
+					case "B":
+                    indexResposta = 1;
+                    break;
+                case "C":
+                    indexResposta = 2;
+                    break;
+                case "D":
+                    indexResposta = 3;
+                    break;
+                default:
+				Imprimir("Resposta inválida. Tente novamente.\n",TimeUnit.MILLISECONDS, tempo_transicao);
+				continue;
+            }
+			
+            if (respostas[indexResposta].equals("2.2° e 3.5°")) {
+				Imprimir("Ethan Blackwood: Consegui! As coordenadas são 2.2° e 3.5° \n\n",TimeUnit.MILLISECONDS, tempo_transicao);
+                respostaCorreta = true;
+            } else {
+                vidas--;  // Decrementa o contador de vidas
+                if (vidas > 0) {
+					Imprimir("Raízes incorretas, tente novamente. Vidas restantes: " + vidas + "\n\n",TimeUnit.MILLISECONDS, tempo_transicao);
+                } else {
+					Imprimir("Você perdeu todas as suas vidas. Tente novamente para ir para o próximo capítulo.",TimeUnit.MILLISECONDS, tempo_transicao);
+                    Menu();
+                }
+            }
+        }
+		
+		Imprimir("Ethan no local indicado pelo mapa, um antigo ponto de referência da cidade.\n\n",TimeUnit.MILLISECONDS, tempo_narrativa);		
+		
+		Imprimir("Ethan: (olhando em volta, segurando o mapa) Se minha interpretação está correta, este é o lugar onde o sol se põe. Agora, só preciso encontrar a entrada secreta.\n\n",TimeUnit.MILLISECONDS, tempo_dialogo);		
+		
+		Imprimir("Após semanas de investigação, noites sem dormir e inúmeros perigos enfrentados, Ethan finalmente decifra o mapa. \n",TimeUnit.MILLISECONDS, tempo_narrativa);		
+		Imprimir("As coordenadas o levam a um local abandonado nos arredores da cidade. Ao chegar lá, ele encontra um baú enterrado, contendo mais documentos, artefatos e outra pista que o levaria a \\\"Aurora\\\"",TimeUnit.MILLISECONDS, tempo_narrativa);		
 	}
 
 //	 Função responsável por iniciar o SEGUNDO capitúlo.
@@ -202,7 +314,7 @@ public class OperacaoAurora {
 		
 		Imprimir("Ethan Blackwood: Bom acho posso ir até o Olho Secreto, espero que eu consiga descobrir toda a verdade.\n\n",TimeUnit.MILLISECONDS, tempo_dialogo);
 
-//		Enigma 2: O Código Secreto:
+		// Enigma 2: O Código Secreto:
 
 		Imprimir("Você chega até a base do olho secreto e consegue se infiltrar na base sem ser visto pelos guardas.\n",TimeUnit.MILLISECONDS, tempo_narrativa);
 		Imprimir("Ao se infiltrar na base você se depara com um computador protegido por um código complexo que proteje a entrada da organização.\n",TimeUnit.MILLISECONDS, tempo_narrativa);
@@ -280,7 +392,6 @@ public class OperacaoAurora {
 	}
 
 	// Função responsável por iniciar o TERCEIRO capitúlo.
-
 	static void BuscarVerdade() throws Exception {
 		Imprimir("\n\nCapitulo 3:\n\n",TimeUnit.MILLISECONDS, tempo_transicao);
 		
@@ -944,15 +1055,15 @@ public class OperacaoAurora {
 
 		case 1:
 			Imprimir("\nVocê escolheu Perdoar Aurora.\n\n",TimeUnit.MILLISECONDS, tempo_transicao);
-			final1PerdoarAurora();
+			FinalPerdoarAurora();
 			break;
 		case 2:
 			Imprimir("\nVocê escolheu Buscar Vingança.\n\n",TimeUnit.MILLISECONDS, tempo_transicao);
-			fina2BuscarVinganca();
+			FinalBuscarVinganca();
 			break;
 		case 3:
 			Imprimir("\nVocê escolheu Uma Nova Jornada.\n\n",TimeUnit.MILLISECONDS, tempo_transicao);
-			fina3UmaNovaJornada();
+			FinalUmaNovaJornada();
 			break;
 		default:
 			Imprimir("\nOpção inválida. Tente novamente!\n\n",TimeUnit.MILLISECONDS, tempo_transicao);
@@ -962,7 +1073,7 @@ public class OperacaoAurora {
 
 	}
 
-	public static void final1PerdoarAurora() throws Exception {
+	public static void FinalPerdoarAurora() throws Exception {
 		Imprimir("Com os primeiros raios de sol iluminando o jardim zen do templo budista, Ethan Blackwood olha profundamente nos olhos de Aurora.\n"//Ethan Blackwood
 				+ "A tensão é palpável, mas há algo mais – um entendimento silencioso. Ele respira fundo, sentindo o peso da decisão iminente. Após um momento \nque parece eterno, ele finalmente fala:\n\n",TimeUnit.MILLISECONDS, tempo_narrativa);
 		
@@ -1003,7 +1114,7 @@ public class OperacaoAurora {
 		
 	}
 
-	public static void fina2BuscarVinganca() throws Exception {
+	public static void FinalBuscarVinganca() throws Exception {
 		
 		Imprimir("Aurora encara Ethan com uma expressão de desafio, seus olhos não revelando nenhum sinal de remorso, apenas uma determinação fria e diz: \n\n",TimeUnit.MILLISECONDS, tempo_narrativa);
 		
@@ -1048,23 +1159,28 @@ public class OperacaoAurora {
 
 	}
 
-	public static void fina3UmaNovaJornada() {
+	public static void FinalUmaNovaJornada() {
 		
 	}
 
 	
 	// #endregion
 
-//	  Função responsável por iniciar o jogo.
+   // Função responsável por iniciar o jogo.
 
 	static void Jogar() throws Exception {
-		// Colocar um letreiro maior (pegar no google) para aparecer no console.
-		Imprimir("SEJA BEM VINDO A OPERAÇÃO AURORA!\n\n",TimeUnit.MILLISECONDS, tempo_transicao);
+		System.out.println("███████╗███████╗     ██╗ █████╗     ██████╗ ███████╗███╗   ███╗    ██╗   ██╗██╗███╗   ██╗██████╗  ██████╗      █████╗      ██████╗ ██████╗ ███████╗██████╗  █████╗  ██████╗      █████╗ ██╗   ██╗██████╗  ██████╗ ██████╗  █████╗ ██╗");
+        System.out.println("██╔════╝██╔════╝     ██║██╔══██╗    ██╔══██╗██╔════╝████╗ ████║    ██║   ██║██║████╗  ██║██╔══██╗██╔═══██╗    ██╔══██╗    ██╔═══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔═══██╗    ██╔══██╗██║   ██║██╔══██╗██╔═══██╗██╔══██╗██╔══██╗██║");
+        System.out.println("███████╗█████╗       ██║███████║    ██████╔╝█████╗  ██╔████╔██║    ██║   ██║██║██╔██╗ ██║██║  ██║██║   ██║    ███████║    ██║   ██║██████╔╝█████╗  ██████╔╝███████║██║   ██║    ███████║██║   ██║██████╔╝██║   ██║██████╔╝███████║██║");
+        System.out.println("╚════██║██╔══╝  ██   ██║██╔══██║    ██╔══██╗██╔══╝  ██║╚██╔╝██║    ╚██╗ ██╔╝██║██║╚██╗██║██║  ██║██║   ██║    ██╔══██║    ██║   ██║██╔═══╝ ██╔══╝  ██╔══██╗██╔══██║██║   ██║    ██╔══██║██║   ██║██╔══██╗██║   ██║██╔══██╗██╔══██║╚═╝");
+        System.out.println("███████║███████╗╚█████╔╝██║  ██║    ██████╔╝███████╗██║ ╚═╝ ██║     ╚████╔╝ ██║██║ ╚████║██████╔╝╚██████╔╝    ██║  ██║    ╚██████╔╝██║     ███████╗██║  ██║██║  ██║╚██████╔╝    ██║  ██║╚██████╔╝██║  ██║╚██████╔╝██║  ██║██║  ██║██╗");
+        System.out.println("╚══════╝╚══════╝ ╚════╝ ╚═╝  ╚═╝    ╚═════╝ ╚══════╝╚═╝     ╚═╝      ╚═══╝  ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝     ╚═╝  ╚═╝     ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝");
 
-		Imprimir("Preparado(a) para uma aventura nunca antes vivida? Aperte os cintos e vamos lá!",
-				TimeUnit.MILLISECONDS, tempo_transicao);
+		Imprimir("Preparado(a) para uma aventura nunca antes vivida? Aperte os cintos e vamos lá!", TimeUnit.MILLISECONDS, tempo_transicao);
 
-//		  Inicializando os capitúlos do jogo e seus respectivos aspectos.
+        /* 
+		 * Inicializando os capitúlos do jogo e seus respectivos aspectos.
+		 */ 
 
 		// 1 - capitulo
 		SombrasGrandeMaca();
@@ -1080,7 +1196,6 @@ public class OperacaoAurora {
 
 		// 5 - capitulo
 		EncontrarAurora();
-
 	}
 
 	// //#endregion
@@ -1142,13 +1257,10 @@ public class OperacaoAurora {
 	 */
 	public static void main(String[] args) throws Exception {
 		// Iniciando o jogo com o menu
-
-		EncontrarAurora();
-//
+		SombrasGrandeMaca();
+		
 //		// Ao final de tudo o scanner é encerrado.
 //		input.close();
-
-		
 
 	}
 }
